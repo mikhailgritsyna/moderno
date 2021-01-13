927e0e8105648f4e97aefe6804520bdbdf156eb0
mikhailgritsyna <corg361@gmail.com>
2021-01-13 14:26:13 +0200

    added product- slider and  product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function(){
     
+  var mixer = mixitup('.products_inner-box');
+
+ 
+
+  $(".rate-star").rateYo({
+    rating: 5,
+    starWidth: "12px"
+  });
+
+  $('.product-slider-inner').slick({
+      dots: true,
+      arrows: false,
+      slidesToShow: 4,
+      slidesToScroll: 4
+  });
+  
 })
